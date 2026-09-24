e80606493f821ed730269c1e5eb97a89551f270b
Anupam Yadav <anupamy571@gmail.com>
2026-09-24 05:30:06 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://user-based-auth.onrender.com",
+  "https://jwt-auth-frontend-iovx.onrender.com",
 ];
 
 app.use(
